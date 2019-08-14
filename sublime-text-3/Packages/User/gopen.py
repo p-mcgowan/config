7c87529dd7aaf9be7d0f30dba16dd152a86df7e5
p-mcgowan@xpress <mcgowanapat@gmail.com>
2019-08-14 17:41:33 +0200

    wp

--- sublime-text-3/Packages/User/gopen.py
+++ sublime-text-3/Packages/User/gopen.py
@@ -3,13 +3,15 @@ import sublime
 import sublime_plugin
 import subprocess
 
+f = open("/home/pat/tmp/gopen.py.log", "w+")
+
 class gopenCommand(sublime_plugin.WindowCommand):
 
   def run(self, app="", args=[], type=None, cli=False, input=False):
     if self.window.active_view():
       fname = self.window.active_view().file_name()
       dirName = os.path.dirname(fname)
-      syscmd = '/usr/bin/gnome-terminal --working-directory="' + dirName + '"; exec /bin/bash -il'
+      syscmd = '/usr/bin/gnome-terminal --working-directory="' + dirName + '" "/bin/bash -il"'
       p = subprocess.Popen(syscmd, shell=True, stdout=subprocess.PIPE)
 
       print(dirName)
