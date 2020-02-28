0fb52522baf04770b04f54f90e0bacb1b857f19d
p-mcgowan@xpress <mcgowanapat@gmail.com>
2020-02-28 10:16:36 +0100

    finally fixed gopen

--- sublime-text-3/Packages/User/gopen.py
+++ sublime-text-3/Packages/User/gopen.py
@@ -3,48 +3,25 @@ import sublime
 import sublime_plugin
 import subprocess
 
-f = open("/home/pat/tmp/gopen.py.log", "w+")
-
 class gopenCommand(sublime_plugin.WindowCommand):
 
-  def run(self, app="", args=[], type=None, cli=False, input=False):
-    if self.window.active_view():
-      fname = self.window.active_view().file_name()
-      dirName = os.path.dirname(fname)
-      syscmd = '/usr/bin/gnome-terminal --working-directory="' + dirName + '" "/bin/bash -il"'
-      p = subprocess.Popen(syscmd, shell=True, stdout=subprocess.PIPE)
+    def run(self, app="", args=[], type=None, cli=False, input=False):
+        if self.window.active_view():
+            fname = self.window.active_view().file_name()
+            dirName = os.path.dirname(fname)
+            syscmd = '/usr/bin/gnome-terminal --working-directory="' + dirName + '" "/bin/bash -il"'
+            env = os.environ.copy()
+            env["GNOME_TERMINAL_SCREEN"] = ""
+            p = subprocess.Popen(syscmd, stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE, env=env, shell=True)
 
-      print(dirName)
-      print(syscmd)
-      print(p)
+            print(syscmd)
+            stdout = p.stdout.read()
+            stderr = p.stdout.read()
 
-  def is_enabled(self):
-    return True
+            if stderr:
+                print(stderr)
+            if stdout:
+                print(stdout)
 
-# import os
-# import sublime
-# import sublime_plugin
-# import subprocess
-
-# f = open("/home/pat/tmp/gopen.py.log", "w+")
-
-# class gopenCommand(sublime_plugin.WindowCommand):
-
-#   def run(self, app="", args=[], type=None, cli=False, input=False):
-#     if self.window.active_view():
-#       fname = self.window.active_view().file_name()
-#       dirName = os.path.dirname(fname)
-#       # syscmd = '/usr/bin/gnome-terminal --working-directory="' + dirName + '" "/bin/bash -il"'
-#       # syscmda = ['/usr/bin/gnome-terminal', '--working-directory="' + dirName + '"', '/bin/bash -il']
-#       syscmd = '/bin/bash /home/pat/.config/sublime-text-3/Packages/User/runterm.sh "' + dirName + '"'
-#       # p = os.system('/bin/bash /home/pat/.config/sublime-text-3/Packages/User/runterm.sh "' + dirName + '"')
-#       # syscmd = '/usr/bin/bash ./runterm.sh "' + dirName + '"'
-#       p = subprocess.Popen(syscmd, shell=True, stdout=subprocess.PIPE)
-#       # p = subprocess.call(syscmd, shell=True, stdout=subprocess.PIPE)
-#       # /home/pat/.config/sublime-text-3/Packages/User/gopen.py
-#       print(dirName)
-#       # print(syscmd)
-#       print(p)
-
-#   def is_enabled(self):
-#     return True
+    def is_enabled(self):
+        return True
