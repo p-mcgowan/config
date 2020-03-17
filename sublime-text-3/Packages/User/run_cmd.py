6b7728586f4613e47db0447bdba7214e292adc34
p-mcgowan@xpress <mcgowanapat@gmail.com>
2020-03-17 17:37:00 +0100

    generic runner

--- sublime-text-3/Packages/User/run_cmd.py
+++ sublime-text-3/Packages/User/run_cmd.py
@@ -2,6 +2,35 @@ import os
 import sublime
 import sublime_plugin
 import subprocess
+import re
+
+class RunOnCommand(sublime_plugin.WindowCommand):
+    """Exec command here with DIRNAME and FILENAME subs."""
+
+    def run(self, command, app="", args=[], type=None, cli=False, input=False):
+        if self.window.active_view():
+            fname = self.window.active_view().file_name()
+
+            dirName = os.path.dirname(fname)
+
+            command = re.sub('DIRNAME', dirName, command)
+            command = re.sub('FILENAME', fname, command)
+
+            env = os.environ.copy()
+            p = subprocess.Popen(command, stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE, env=env, shell=True)
+
+            print(command)
+
+            stdout = p.stdout.read()
+            stderr = p.stdout.read()
+
+            if stderr:
+                print(stderr)
+            if stdout:
+                print(stdout)
+
+    def is_enabled(self):
+        return True
 
 class RunCmdCommand(sublime_plugin.WindowCommand):
     """Exec command here."""
