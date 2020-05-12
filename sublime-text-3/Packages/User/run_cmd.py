6b2c41a2c7893dd4a37ca42b47801ba0ac4b8ec6
p-mcgowan <mcgowanapat+git@gmail.com>
2020-05-12 13:53:37 +0200

    wip

--- sublime-text-3/Packages/User/run_cmd.py
+++ sublime-text-3/Packages/User/run_cmd.py
@@ -62,6 +62,8 @@ class RunCmdCommand(sublime_plugin.WindowCommand):
         if self.window.active_view():
             fname = self.window.active_view().file_name()
             dirName = os.path.dirname(fname)
+            command = re.sub('DIRNAME', dirName, command)
+            command = re.sub('FILENAME', fname, command)
             syscmd = 'cd ' + dirName + ' && ' + command
             env = os.environ.copy()
             p = subprocess.Popen(syscmd, stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE, env=env, shell=True)
