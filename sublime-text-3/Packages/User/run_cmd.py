9e698f91fca9c651b42eb953ff9f41b90050ee85
Patrick McGowan <patrick.mcgowan@acrontum.de>
2020-08-13 18:14:40 +0200

    wip

--- sublime-text-3/Packages/User/run_cmd.py
+++ sublime-text-3/Packages/User/run_cmd.py
@@ -19,64 +19,43 @@ errorTemplate = Template("""
 # <style>body { height: 100%; width: 100%; } div { margin: 5px; white-space: nowrap; }</style>
 # <div>${error}</div>
 
-class RunOnCommand(sublime_plugin.WindowCommand):
-    """Exec command here with DIRNAME and FILENAME subs."""
-
-    def run(self, command, app="", args=[], type=None, cli=False, input=False):
-        if self.window.active_view():
-            fname = self.window.active_view().file_name()
-
-            dirName = os.path.dirname(fname)
-
-            command = re.sub('DIRNAME', dirName, command)
-            command = re.sub('FILENAME', fname, command)
-
-            # running gnome-terminal fails sometimes
-            env = os.environ.copy()
-            env["GNOME_TERMINAL_SCREEN"] = ""
-            p = subprocess.Popen(command, stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE, env=env, shell=True)
-
-            print(command)
-
-            stdout = p.stdout.read()
-            stderr = p.stderr.read()
-
-            if stderr:
-                content = ''.join(map(lambda x: '<div><code>' + re.sub(' ', '&nbsp;', x) + '</code></div>', stderr.decode('UTF-8').split('\n')))
-                print(content)
-                content = errorTemplate.substitute({ "error": content })
-                max_width = min(self.window.active_view().viewport_extent()[0], 800)
-                # self.window.active_view().show_popup(content, max_width=self.window.active_view().viewport_extent()[0], max_height=400)
-                self.window.active_view().show_popup(content, max_width=max_width, max_height=400)
-                print(stderr.decode('UTF-8'))
-            if stdout:
-                print(stdout)
-
-    def is_enabled(self):
-        return True
-
 class RunCmdCommand(sublime_plugin.WindowCommand):
     """Exec command here."""
 
     def run(self, command, app="", args=[], type=None, cli=False, input=False):
         if self.window.active_view():
             fname = self.window.active_view().file_name()
+
             dirName = os.path.dirname(fname)
+
             command = re.sub('DIRNAME', dirName, command)
             command = re.sub('FILENAME', fname, command)
-            syscmd = 'cd ' + dirName + ' && ' + command
+
+            # command = 'cd ' + dirName + ' && ' + command
+
+
+            # running gnome-terminal fails sometimes
             env = os.environ.copy()
-            p = subprocess.Popen(syscmd, stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE, env=env, shell=True)
+            env["GNOME_TERMINAL_SCREEN"] = ""
+            p = subprocess.Popen(command, stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE, env=env, shell=True, cwd=dirName)
 
-            print(syscmd)
+            stdout, stderr = p.communicate()
+            exitCode = p.wait()
 
-            stdout = p.stdout.read()
-            stderr = p.stdout.read()
+            print('$ {0} => {1}'.format(command, exitCode))
 
             if stderr:
-                print(stderr)
+                content = ''.join(map(lambda x: '<div><code>' + re.sub(' ', '&nbsp;', x) + '</code></div>', stderr.decode('UTF-8').split('\n')))
+                print('SOMETHING WENT WRONG:')
+                print(content)
+                content = errorTemplate.substitute({ "error": content })
+                max_width = min(self.window.active_view().viewport_extent()[0], 800)
+                # self.window.active_view().show_popup(content, max_width=self.window.active_view().viewport_extent()[0], max_height=400)
+                self.window.active_view().show_popup(content, max_width=max_width, max_height=400)
+                print(stderr.decode('UTF-8'))
+
             if stdout:
-                print(stdout)
+                print(stdout.decode('UTF-8'))
 
     def is_enabled(self):
         return True
