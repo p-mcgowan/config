0d201f744aa2c6ba9fdafe8ca62f1f2ee7043eaa
Patrick McGowan <patrick.mcgowan@acrontum.de>
2020-11-30 18:55:53 +0100

    add wrap plugin, fix up jest and group commands, rebind keys

--- sublime-text-3/Packages/User/move_to_new_group_command.py
+++ sublime-text-3/Packages/User/move_to_new_group_command.py
@@ -2,7 +2,9 @@ import sublime, sublime_plugin
 
 # https://forum.sublimetext.com/t/set-layout-reference/5713
 class MoveToNewGroupCommand(sublime_plugin.WindowCommand):
-    def run(self):
-        self.window.run_command("set_layout", {"cells": [[0, 0, 1, 1], [1, 0, 2, 1]], "cols": [0.0, 0.5, 1.0], "rows": [0.0, 1.0]})
-        self.window.run_command("focus_group", {"group": 0})
-        self.window.run_command("move_to_group", {"group": 1})
+    def run(self, group = 1):
+        print("group {}".format(group))
+        if (len(self.window.get_layout()["cells"]) <= group):
+            self.window.set_layout({"cells": [[0, 0, 1, 1], [1, 0, 2, 1]], "cols": [0.0, 0.5, 1.0], "rows": [0.0, 1.0]})
+        self.window.run_command("move_to_group", {"group": group, "focus": True})
+        self.window.focus_group(group)
