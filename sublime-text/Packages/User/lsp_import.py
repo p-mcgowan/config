4b5bc07959b5feaa71a4bdf5913d54b741d3ec9f
p-mcgowan <mcgowanapat+git@gmail.com>
2021-08-22 11:56:55 +0200

    use smart select

--- sublime-text/Packages/User/lsp_import.py
+++ sublime-text/Packages/User/lsp_import.py
@@ -3,5 +3,8 @@ import sublime_plugin
 class LspImportCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
-        self.view.run_command("move", { "by": "word_ends", "forward": "true" })
+        for region in self.view.sel():
+            if region.empty():
+                self.view.run_command("expand_selection", {"to": "smart"})
+                break
         self.view.run_command("auto_complete")
