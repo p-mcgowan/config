6532fa3b850e31c40ee8bb3c6f4ee228e75ae03b
p-mcgowan@winbot <winbot@patmcgowan.dev>
2022-08-28 00:33:20 +0200

    add move to previous pane hotkey command

--- sublime-text-3/Packages/User/move_to_new_group_command.py
+++ sublime-text-3/Packages/User/move_to_new_group_command.py
@@ -8,3 +8,8 @@ class MoveToNewGroupCommand(sublime_plugin.WindowCommand):
             self.window.set_layout({"cells": [[0, 0, 1, 1], [1, 0, 2, 1]], "cols": [0.0, 0.5, 1.0], "rows": [0.0, 1.0]})
         self.window.run_command("move_to_group", {"group": group, "focus": True})
         self.window.focus_group(group)
+
+class MoveToOldGroupCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        self.window.run_command("focus_group", {"group": 1})
+        self.window.run_command("move_to_group", {"group": 0})
